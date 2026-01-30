bd5f5c4090657577e165dce9248c011b2d35a7a0
Cameron Jasper <camjasper1@gmail.com>
2026-01-29 20:20:04 -0600

    set up for Login prefill Code

---
+++ js/loginPreFill.js
@@ -0,0 +1,5 @@
+document.getElementById('btnClientLogin').addEventListener('click', function() 
+{
+    const modal = new bootstrap.Modal(document.getElementById('prefillPopup'));
+    modal.show();
+});
