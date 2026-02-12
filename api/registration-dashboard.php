cf463b755c7f287f82d5580ca46a2a5b13e4216b
Radman4 <nootnoot432@gmail.com>
2026-02-12 17:59:56 -0600

    added select query to Registration dashboard
    will still need to add the test needed to get every instance of a user in the sql query

--- api/registration-dashboard.php
+++ api/registration-dashboard.php
@@ -11,6 +11,36 @@ if ($_SERVER['CONTENT_TYPE'] === 'application/json') {
 // Get set mysql connection
 $mysqli = $GLOBALS['mysqli'];
 
-// tldr: join from clientid to clients, then send to javascript 
+// Single query to get all client data
+$registrationData = $mysqli->prepare("
+    SELECT 
+        c.ClientID, c.FirstName, c.MiddleInitial, c.LastName, c.DateCreated, c.DOB, c.Sex, c.Phone,
+        a.Street1, a.Street2, a.City, a.State, a.ZIP,
+        e.Name AS EmergencyName, e.Phone AS EmergencyPhone,
+        r.DateTime AS RegistrationDate, r.Medical, r.Optical, r.Dental, r.Hair
+    FROM tblClients c
+    LEFT JOIN tblClientAddress a ON c.ClientID = a.ClientID
+    LEFT JOIN tblClientEmergencyContacts e ON c.ClientID = e.ClientID
+    LEFT JOIN tblClientRegistrations r ON c.ClientID = r.ClientID
+    WHERE c.ClientID = ?
+");
 
-echo "somthing";
+$registrationData->bind_param("i", $clientId);
+$registrationData->execute();
+$result = $registrationData->get_result();
+$userData = $result->fetch_assoc();
+$registrationData->close();
+
+// Return data
+if ($userData) {
+    http_response_code(200);
+    $msg = json_encode(['success' => true, 'message' => 'Data retrieved successfully.', 'data' => $userData]);
+} else {
+    http_response_code(404);
+    $msg = json_encode(['success' => false, 'message' => 'Client not found.']);
+}
+
+echo $msg;
+error_log($msg);
+
+echo "making sure it still works!";
