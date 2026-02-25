e3de783b86198b99cc30cc93df678652813dcede
Radman4 <nootnoot432@gmail.com>
2026-02-24 20:38:11 -0600

    changed waiting-room.php to be about getting equation data for the query

--- api/waiting-room.php
+++ api/waiting-room.php
@@ -7,8 +7,8 @@
  *               scnarios such as registration dashboard.
  *
  *  Last Modified By:  Miguel
- *  Last Modified On:  Feb 23 @ 9:38 PM
- *  Changes Made:      Created
+ *  Last Modified On:  Feb 24 @ 3:31 PM
+ *  Changes Made:      added steps 2 and 3
  * ============================================================
 */
 
@@ -22,101 +22,44 @@ header('Content-Type: application/json');
 $mysqli = $GLOBALS['mysqli'];
 
 
-/*This task is for determining the order of the clients in the waiting room queue list table (priority level)
-and sending it back to the JS to actually put them in the table. (AKA, the algorithm) 
-
-We need three variables to consider when it come to queueing: 
-    the total time they have been to the event 
-        (which is determined by the first time they were finalized at checked in(aka registration dashboard)),
-    how long they have been in the wait room for 
-        (needs to update every time a service is finished, for example, if I get out of medical,
-        my wait room time should be the exact time I got out), DONE
-    and if the service is currently not full of clients receiving treatment 
-        (aka if only 4 people can be worked on in dental, it shouldn't be selecting a person for dental
-         to be treated right now because there are no empty seats). DONE
-
-Another thing, split the files for the endpoints.
-Change this file to registration-dashboard.php and then make a new one specifically for the waiting room code called waiting-room.php.
-I know that we currently track the datetime for when a client is checked into the waiting room, 
-but let me know if a DB addition is needed for initial time checked in to compare for the algorithm.*/
-
-//total time @ event
-
+// https://www.notion.so/Queue-Algorithm-Logic-312f042b2de18052a8d6e7eff2c4b3bf?source=copy_link
+//notion link for skyler's request
+/*
 
 /*
 ---------------------------------
-         PART 2
-Client waiting time check
+         PART 1
+The Operation Query
 ----------------------------------
 */
 
-//how long they have been @ wait room (this currently gets anyone who is 30 minutes past their check in time)
-$clientTimeSelect = $mysqli->prepare("
-    SELECT c.ClientID, c.FirstName, c.LastName, v.CheckInTime
+$QueueScoreSelect = $mysqli->prepare(
+    "SELECT 
+    c.ClientID, 
+    c.FirstName, 
+    c.LastName, 
+    c.DOB, 
+    v.FirstCheckedIn, 
+    v.EnteredWaitingRoom, 
+    s.ServiceID,
+    e.EventDate - NOW() as TotalEventTime,
+	v.EnteredWaitingRoom - v.FirstCheckedIn as CurrentTimeSpent,
+    (v.EnteredWaitingRoom - v.FirstCheckedIn) + (0.5 * (e.EventDate - NOW())) AS QueueScore
     FROM tblVisits v
     JOIN tblClients c ON v.ClientID = c.ClientID
-    WHERE v.CheckInTime < DATE_SUB(NOW(), INTERVAL 30 MINUTE)"
+    JOIN tblEvents e ON e.EventID = v.EventID
+    JOIN tblVisitServiceSelections s ON s.ClientID = c.ClientID
+    order by QueueScore ASC"
 );
-$clientTimeSelect->execute();
-$clientTimes = $clientTimeSelect->get_result()->fetch_all(MYSQLI_ASSOC);
-$clientTimeSelect->close();
+$QueueScoreSelect->execute();
+$QueueQuery = $QueueScoreSelect->get_result()->fetch_all(MYSQLI_ASSOC);
+$QueueScoreSelect->close();
 echo json_encode([
-    "ClientTimes"      => $clientTimes
+    "QueueScore"      => $QueueQuery
 ]);
-
 
 /*
 ---------------------------------
          PART 3
 Check for if service is full
 ----------------------------------
-*/
-
-//check if queue is full
-if (!isset($_GET['ServiceID'])) {
-    echo json_encode(["error" => "Missing ServiceID"]);
-    exit;
-}
-
-$serviceID = $_GET['ServiceID'];
-
-// query to check which type of service is selected
-$serviceSelect = $mysqli->prepare("
-    SELECT MaxCapacity, CurrentAssigned, IsClosed
-    FROM tblEventServices
-    WHERE ServiceID = ?"
-);
-$serviceSelect->bind_param("s", $serviceID);
-$serviceSelect->execute();
-$service = $serviceSelect->get_result()->fetch_assoc();
-$serviceSelect->close();
-
-if (!$service) {
-    echo json_encode(["error" => "Service not found"]);
-    exit;
-}
-
-
-// check to see if service is full
-$isFull = $service['CurrentAssigned'] >= $service['MaxCapacity'];
-
-if ($isFull && $service['IsClosed'] == 0) {
-    $fullCheck = $mysqli->prepare("
-        UPDATE tblEventServices
-        SET IsClosed = 1
-        WHERE ServiceID = ?"
-    );
-    $fullCheck->bind_param("s", $serviceID);
-    $fullCheck->execute();
-    $fullCheck->close();
-
-    $service['IsClosed'] = 1;
-}
-
-echo json_encode([
-    "ServiceID"        => $serviceID,
-    "MaxCapacity"      => $service['MaxCapacity'],
-    "CurrentAssigned"  => $service['CurrentAssigned'],
-    "IsClosed"         => $service['IsClosed'],
-    "IsFull"           => $isFull
-]);
