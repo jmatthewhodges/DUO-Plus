e39efe32f400e35ca499eec30eb446744b98ac3c
Radman4 <nootnoot432@gmail.com>
2026-03-02 12:52:19 -0600

    Create ServiceSelection.php

---
+++ api/ServiceSelection.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * ============================================================
+ *  File:        ServiceSelection.php
+ *  Purpose:     API endpoint for verifying PIN code for access restriction
+ * 
+ *  Last Modified By:  Miguel
+ *  Last Modified On:  Mar 2 @ 12:45 PM
+ *  Changes Made:      Created
+ * ============================================================
+*/
+/*
+endpoints 
+all clients checked in or in progress for medical
+number of people in waitlist
+number in progress
+completed
+avg wait time
+past avg service time
+
+webpage for all info http://localhost:8000/pages/service-scan.html
+*/
