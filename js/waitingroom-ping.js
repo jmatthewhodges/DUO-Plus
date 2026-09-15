52c5c037ac18696361833605ff1b262cc650cb77
Cameron Jasper <camjasper1@gmail.com>
2026-09-14 22:33:45 -0500

    Initial file creation

---
+++ js/waitingroom-ping.js
@@ -0,0 +1,10 @@
+/**
+ * ============================================================
+ * File:            waitroom-ping.js
+ * Description:     Allows service scan pages to request clients from waitng room.
+ *
+ * Last Modified By:  Cameron
+ * Last Modified On:  Sept 14 @ 11:00 PM
+ * Changes Made:      Initial File Creation
+ * ============================================================
+ */
